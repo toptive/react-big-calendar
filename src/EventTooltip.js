ad1e5b625aa40779af7466d577f9a66f9f97bc06
sbaldani <sbaldani@toptive.co>
2021-10-19 17:47:23 -0300

    created EventTooltipcomponent

---
+++ src/EventTooltip.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class EventTooltip extends React.Component {
+  render() {
+    return (
+      <div style={{ position: 'absolute' }}>
+        <h3>render subfields</h3>
+      </div>
+    )
+  }
+}
+
+export default EventTooltip
